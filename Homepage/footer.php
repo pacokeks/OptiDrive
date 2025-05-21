2cdcde976e1819b9264a7f98508a01f1f1bbc908
seki <seki@gmail.com>
2025-05-21 10:44:07 +0200

    initial commit

---
+++ Homepage/footer.php
@@ -0,0 +1,6 @@
+<footer class="text-center py-4 bg-light">
+    <p>&copy; <?php echo date("Y"); ?> Picar-X Projekt</p>
+</footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
